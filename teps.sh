4d16b3c5b39cb70aa745b05b6f024aa8d03c78d7
Leonardo Santos <27875029+s4nt0sl30n4rd0@users.noreply.github.com>
2018-06-21 17:03:22 -0300

    Starting the project.

---
+++ teps.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# ScriptBash.sh - Serve de modelo para todos os scripts criados.
+#
+# Autor:
+#	Leonardo Santos <27875029+s4nt0sl30n4rd0@users.noreply.github.com>
+#
+# Manutenção:
+#	Leonardo Santos <27875029+s4nt0sl30n4rd0@users.noreply.github.com>
+#
+# -------------------------------------------------------------------------
+#
+# Este programa não executa nada, apenas serve como modelo para a criação
+# de scripts.
+#
+# -------------------------------------------------------------------------
+#
+# Licença:
+#
+#	General Public License 3.0
+#
+# -------------------------------------------------------------------------
+
+

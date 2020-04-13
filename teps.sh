593f4512cda2ac233e3bccb5a84b6669cf52b052
Leonardo Santos <27875029+s4nt0sl30n4rd0@users.noreply.github.com>
2020-04-13 18:51:59 -0300

    teps: created the '-h' option message.

--- teps.sh
+++ teps.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+#
+# teps - Troca Espaço Por Sublinhado.
+#
+# Author:
 #	Leonardo Santos
 #	<27875029+s4nt0sl30n4rd0@users.noreply.github.com>
 #
@@ -20,9 +25,23 @@
 #
 # -------------------------------------------------------------------------
 
-#TODO Colocar dentro do if da toRename, antes do while
+usage(){
+	cat << EOF
+Uso: $(basename $0) [-s|-t [DIR]] [-h]
+     Exchange Space for Underline.
 
-#TODO Criar variável que guardará o sublinhado ou traço
+     Exchanges spaces for underscores in file and directory names
+     recursively within the given directory, including itself.
+     If a name or path to a directory is not entered as a parameter,
+     it will act in the current directory.
+
+     Options:
+       -s       explicitly define the underscore to replace spaces in names.
+                The same as without informed option
+       -t       define the dash to replace spaces in names
+       -h       display this help and exit
+EOF
+}
 
 toRename(){
 	cd "$local" 2> /dev/null
@@ -40,6 +59,8 @@ toRename(){
 		exit 1
 	fi
 }
+
+#TODO Criar variável que guardará o sublinhado ou traço
 
 #TODO Implementar opções para traço e help
 if [ -n "$1" ]
